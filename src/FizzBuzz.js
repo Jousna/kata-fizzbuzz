928f68d3694f2426f895bfd8d6964cbccc0f6605
thoni130 <anthonyreboli@hotmail.fr>
2015-11-24 17:05:22 +0100

    resultat

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,5 +1,22 @@
 function fizzBuzz(i) {
-  
+
+if (i % 15 === 0){
+return "FizzBuzz";
 }
+else if (i % 3 === 0){ 
+return "Fizz";
+}
+
+else if (i % 5 === 0) {
+  return "Buzz";
+}
+else {
+  return (i);
+}
+}
+
+
+
+
 
 module.exports = fizzBuzz;
